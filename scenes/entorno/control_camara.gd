ef60f81453bbb3059a900fdd617aa1efe0150bfe
Luxa <luxamgames2000@gmail.com>
2026-04-15 19:38:05 -0300

    cambios de camara, perfectos y con limites

--- scenes/entorno/control_camara.gd
+++ scenes/entorno/control_camara.gd
@@ -16,11 +16,26 @@ var rapidez_zoom = 0.3
 
 var direccion_movimiento
 var rapidez_movimiento : float = 15.0
+var rotacion_actual : Vector2 = Vector2(-53, -46) 
 func _ready() -> void:
 	#suave
 	pivot_y.top_level = true
+	reiniciar_posicion()
+	
+func reiniciar_posicion():
+	camera_3d.position.z = 9.7
+	position = Vector3(3.39, 0, 26.48)
+	pivot_y.rotation_degrees =  Vector3(rotacion_actual.x, rotacion_actual.y, 0)
 	
 func _input(event) -> void:
+	if Input.is_key_pressed(KEY_SPACE):
+		
+		var info_texto = "CÁMARA DEBUG\n"
+		info_texto += "Posición: " + str(position.snapped(Vector3(0.01, 0.01, 0.01))) + "\n"
+		info_texto += "Rotación: " + str(pivot_y.rotation_degrees.snapped(Vector3(0.1, 0.1, 0.1))) + "\n"
+		info_texto += "Zoom: " + str(camera_3d.position.z).pad_decimals(1) + "\n"
+		print(info_texto)
+		
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT: # girar camara
 			boton_mouse_rueda_presionada = event.pressed 
@@ -33,9 +48,9 @@ func _input(event) -> void:
 		if boton_mouse_rueda_presionada:
 			pivot_y.rotation_degrees.y -= event.relative.x * rapidez_rotacion # se puede poner + para que gire al reves
 			pivot_x.rotation_degrees.x -= event.relative.y * rapidez_rotacion
-			pivot_x.rotation_degrees.x = clamp(pivot_x.rotation_degrees.x,-80,10)
+			pivot_x.rotation_degrees.x = clamp(pivot_x.rotation_degrees.x,-50,39)
 			
 func _physics_process(delta: float) -> void:
 	direccion_movimiento = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	global_position += Vector3(direccion_movimiento.x,0,direccion_movimiento.y).rotated(Vector3.UP, pivot_y.rotation.y) * rapidez_movimiento * delta
-	pivot_y.global_position = (global_position + pivot_y.global_position) * 0.1
+	pivot_y.global_position = (global_position + pivot_y.global_position) * 0.5
