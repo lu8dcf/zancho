b699856f1fa88383338638ea12f30883f0953356
Violeta Mortarini Forchetti <violemf@gmail.com>
2026-04-17 00:08:55 -0300

    Creacion de oleadas y conexion.
    Agrego una base data de la estructura por niveles en las oleadas. Conexion cambio de nivel.

--- scenes/levels/oleada.gd
+++ scenes/levels/oleada.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+var pausarOleada :bool = false
+var estadoOleada :bool = false
+var DATA_OLEADAS = preload("res://scenes/levels/data_oleada.gd").new()
 
 # este archivo muestra como crear las piezas en el tablero
 func _ready() -> void:
@@ -7,18 +10,31 @@ func _ready() -> void:
 	# Sitio Vector2 (x,z) el 0,0 esta en el extremo izq  el 15,15 esta en el extremo derecho
 	# Tipo  0-Rey  1-Peon  2-Alfil  3-Torre  4-Caballo  5- Reina
 	# Blanca  true piezas blancas   -  False Piezas negras
-	
 	GlobalSignal.emit_signal("crearPieza",Vector2i(1,14),0,true) #rey blanco en posicion
+	globalJuego.oleada_cambiada.connect(ejecuto_oleada,get_oleada_actual())
+	#globalJuego.siguiente_oleada() #Testeo de conexion, funciona.
+
+	#GlobalSignal.marcaPaso.connect(_testeo_oleada)
+	#if !(pausarOleada):
+		#ejecuto_oleada(globalJuego.oleada_actual)
+
+func get_oleada_actual():
+	return globalJuego.oleada_actual
 	
-	GlobalSignal.emit_signal("crearPieza",Vector2i(2,6),1,true) #peon blanco en posicion
-	GlobalSignal.emit_signal("crearPieza",Vector2i(3,9),2,true) #rey blanco en posicion
-	GlobalSignal.emit_signal("crearPieza",Vector2i(4,13),3,true) #torre blanco en posicion
-	GlobalSignal.emit_signal("crearPieza",Vector2i(7,10),4,true) #caballo blanco en posicion
-	GlobalSignal.emit_signal("crearPieza",Vector2i(9,13),5,true) #reina blanco en posicion
+func ejecuto_oleada(nivel):
+	if !DATA_OLEADAS.estructura_por_nivel.has(nivel):
+		print("Ese nivel no esta desarrollado.")
+		return
+	#GlobalSignal.emit_signal("controlMarcaPaso",true) #tiempo!
+	if estadoOleada:# que no se ejecute una oleada dentro de oleada
+		return
+	estadoOleada = true
+	for pieza in DATA_OLEADAS.estructura_por_nivel[nivel]:
+		GlobalSignal.emit_signal("crearPieza",pieza["pos"],pieza["tipo"], pieza["blancas"])
+	estadoOleada = false
 	
-	#negras
-	GlobalSignal.emit_signal("crearPieza",Vector2i(6,6),1,false) #peon blanco en posicion
-	GlobalSignal.emit_signal("crearPieza",Vector2i(6,9),2,false) #rey blanco en posicion
-	GlobalSignal.emit_signal("crearPieza",Vector2i(6,13),3,false) #torre blanco en posicion
-	GlobalSignal.emit_signal("crearPieza",Vector2i(6,10),4,false) #caballo blanco en posicion
-	GlobalSignal.emit_signal("crearPieza",Vector2i(7,13),5,false) #reina blanco en posicion
+	
+func detenerOleada():
+	pausarOleada =true
+	GlobalSignal.emit_signal("controlMarcaPaso",false)
+	
