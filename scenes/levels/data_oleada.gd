3e95d265917e9247bea27e26e7994d7aaaf9bc58
Violeta Mortarini Forchetti <violemf@gmail.com>
2026-04-22 16:33:01 -0300

    Cambio de posición enemigos

--- scenes/levels/data_oleada.gd
+++ scenes/levels/data_oleada.gd
@@ -3,24 +3,23 @@ class_name OleadasData
 
 static var estructura_por_nivel = {
 	1: [
-		{"pos": Vector2i(6,6), "tipo":1, "blancas":false},
-		{"pos": Vector2i(2,6), "tipo":1, "blancas":false}
+		{"pos": Vector2i(2,11), "tipo":1, "blancas":false},
+		{"pos": Vector2i(5,14), "tipo":1, "blancas":false}
 	],
 	2: [
-		{"pos": Vector2i(3,9), "tipo":1, "blancas":false},
-		{"pos": Vector2i(7,4), "tipo":1, "blancas":false},
-		{"pos": Vector2i(10,5), "tipo":1, "blancas":false},
-		{"pos": Vector2i(14,7), "tipo":1, "blancas":false}
+		{"pos": Vector2i(0,11), "tipo":1, "blancas":false},
+		{"pos": Vector2i(3,15), "tipo":1, "blancas":false},
+		{"pos": Vector2i(0,15), "tipo":1, "blancas":false},
+		{"pos": Vector2i(5,12), "tipo":1, "blancas":false}
 	],
 	3: [
-		{"pos": Vector2i(6,6), "tipo":3, "blancas":false},
-		{"pos": Vector2i(2,6), "tipo":1, "blancas":false}
+		{"pos": Vector2i(4,15), "tipo":3, "blancas":false},
+		{"pos": Vector2i(3,10), "tipo":1, "blancas":false}
 	],
 	4: [
-		{"pos": Vector2i(3,9), "tipo":1, "blancas":false},
-		{"pos": Vector2i(5,6), "tipo":1, "blancas":false},
-		{"pos": Vector2i(14,7), "tipo":1, "blancas":false},
-		{"pos": Vector2i(11,5), "tipo":1, "blancas":false},
-		{"pos": Vector2i(8,6), "tipo":3, "blancas":false}
+		{"pos": Vector2i(0,8), "tipo":1, "blancas":false},
+		{"pos": Vector2i(3,10), "tipo":2, "blancas":false},
+		{"pos": Vector2i(7,14), "tipo":1, "blancas":false},
+		{"pos": Vector2i(3,14), "tipo":3, "blancas":false}
 	]
 }
