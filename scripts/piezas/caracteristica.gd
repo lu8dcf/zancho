17ce954269a7fde760c0573606d9289f9bfdea34
Heber Alonso <84638715+lu8dcf@users.noreply.github.com>
2026-05-06 13:00:57 -0300

    tres de caballo y rey , agregado escena

--- scripts/piezas/caracteristica.gd
+++ scripts/piezas/caracteristica.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name PiezaDato
 
-
+@export var escena : PackedScene
 @export var nombre: String
 @export var vida: int
 @export var danio: int
@@ -9,4 +9,6 @@ class_name PiezaDato
 @export var tipo: int
 @export var blanco: int
 
-@export var ataque: Array = []
+@export var ataque: Array[Vector3i]
+
+@export var altura: int
