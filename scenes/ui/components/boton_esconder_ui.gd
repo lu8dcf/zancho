53221aa93f565c1c6233518b6fde4fcbd93612db
RAX0909 <ramiroelian2003@gmail.com>
2026-05-19 10:24:44 -0300

    cambios en hud

---
+++ scenes/ui/components/boton_esconder_ui.gd
@@ -0,0 +1,37 @@
+extends TextureButton
+
+@export var texto :String
+@onready var label: Label = $Label
+@export var esconder_idle: CompressedTexture2D
+@export var esconder_hover: CompressedTexture2D
+@export var mostrar_hover: CompressedTexture2D
+@export var mostrar_idle: CompressedTexture2D
+
+
+var mostrando : bool = true
+
+func _ready() -> void:
+	label.text = texto
+
+	pressed.connect(_on_pressed)
+	
+
+	texture_normal = mostrar_idle
+	texture_hover = mostrar_hover
+
+func _on_pressed() -> void:
+
+	mostrando = !mostrando
+	
+	if mostrando:
+		texture_normal = mostrar_idle
+		texture_hover = mostrar_hover
+	else:
+		texture_normal = esconder_idle
+		texture_hover = esconder_hover
+
+func cambiar_texto(texto_nuevo:String) :
+	label.text = texto_nuevo
+	
+func ver_texto():
+	return label.text
