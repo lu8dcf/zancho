1b857a5d0c210a82355dc90dba184f8c4db06a28
Heber Alonso <84638715+lu8dcf@users.noreply.github.com>
2026-06-02 01:19:18 -0300

    cambio de materia con señal global

--- assets/modelos/pieza_generica/pieza_1.gd
+++ assets/modelos/pieza_generica/pieza_1.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
 
-@onready var circulo_mesh: MeshInstance3D = $"Esqueleto_008/Skeleton3D/Círculo_037"
+@onready var circulo_mesh: MeshInstance3D = $"Esqueleto/Skeleton3D/Círculo_001"
 
 
 @onready var material_bueno: Material = preload("res://assets/modelos/texturas/material_buenos.tres")
-@onready var material_muerte: Material = preload("res://assets/modelos/texturas/material_malos.tres")
+@onready var material_muerte: Material = preload("res://assets/modelos/texturas/material_muerte.tres")
 
 
 var usando_material_muerte: bool = false
@@ -15,27 +15,14 @@ func _ready() -> void:
 		
 		var mat_unico = material_bueno.duplicate(true) as ShaderMaterial
 		circulo_mesh.material_override = mat_unico
+	GlobalSignal.connect("piezaMuere",cambiar_a_material_muerte)	
 		
+
+func cambiar_a_material_muerte(id) -> void:
 		
-
-func _unhandled_input(event: InputEvent) -> void:
-
-	if event is InputEventKey and event.pressed and event.keycode == KEY_G:
-		cambiar_a_material_muerte()
-
-func cambiar_a_material_muerte() -> void:
-	usando_material_muerte = !usando_material_muerte
+	var material_a_usar = material_muerte
 	
-	var material_a_usar: Material
-	if usando_material_muerte:
-		material_a_usar = material_muerte
-		
-	else:
-		material_a_usar = material_bueno
-		
-
 	if material_a_usar:
-		
 		var mat_nuevo_unico = material_a_usar.duplicate(true) as ShaderMaterial
 		circulo_mesh.material_override = mat_nuevo_unico
 		
