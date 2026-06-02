f2f44bd352b0282cae483dfb30f3874ddc1998f2
Violeta Mortarini Forchetti <violemf@gmail.com>
2026-06-01 21:07:10 -0300

    PresentacionPeones

--- scenes/levels/data_oleada.gd
+++ scenes/levels/data_oleada.gd
@@ -10,23 +10,27 @@ static var estructura_por_nivel = {
 		{"pos": Vector2i(14,8), "tipo":3, "blancas":false}
 	],
 	1: [
-		{"pos": Vector2i(11,2), "tipo":1, "blancas":false},
-		{"pos": Vector2i(14,5), "tipo":1, "blancas":false}
+		{"pos": Vector2i(9,5), "tipo":1, "blancas":false},
+		{"pos": Vector2i(10,6), "tipo":1, "blancas":false}
 	],
 	2: [
-		{"pos": Vector2i(11,0), "tipo":1, "blancas":false},
-		{"pos": Vector2i(15,3), "tipo":1, "blancas":false},
-		{"pos": Vector2i(15,0), "tipo":1, "blancas":false},
-		{"pos": Vector2i(12,5), "tipo":1, "blancas":false}
+		{"pos": Vector2i(8,3), "tipo":1, "blancas":false},
+		{"pos": Vector2i(12,7), "tipo":1, "blancas":false},
+		{"pos": Vector2i(10,5), "tipo":1, "blancas":false}
 	],
 	3: [
-		{"pos": Vector2i(15,4), "tipo":3, "blancas":false},
-		{"pos": Vector2i(10,3), "tipo":1, "blancas":false}
+		{"pos": Vector2i(8,1), "tipo":1, "blancas":false},
+		{"pos": Vector2i(8,2), "tipo":1, "blancas":false},
+		{"pos": Vector2i(12,3), "tipo":1, "blancas":false},
+		{"pos": Vector2i(13,7), "tipo":1, "blancas":false},
+		{"pos": Vector2i(14,7), "tipo":1, "blancas":false}
 	],
 	4: [
-		{"pos": Vector2i(8,0), "tipo":1, "blancas":false},
-		{"pos": Vector2i(10,3), "tipo":2, "blancas":false},
-		{"pos": Vector2i(14,7), "tipo":1, "blancas":false},
-		{"pos": Vector2i(14,3), "tipo":3, "blancas":false}
+		{"pos": Vector2i(9,0), "tipo":1, "blancas":false},
+		{"pos": Vector2i(15,6), "tipo":1, "blancas":false},
+		{"pos": Vector2i(13,1), "tipo":1, "blancas":false},
+		{"pos": Vector2i(14,2), "tipo":1, "blancas":false},
+		{"pos": Vector2i(9,4), "tipo":1, "blancas":false},
+		{"pos": Vector2i(12,6), "tipo":1, "blancas":false}
 	]
 }
