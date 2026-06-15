ed727b22072969a7ef6c549992c44613340ec718
Luxa <luxamgames2000@gmail.com>
2026-06-15 20:08:58 -0300

    cambie el tipo de dato en accesibilidad

--- scenes/ui/accesibilidad_menu.gd
+++ scenes/ui/accesibilidad_menu.gd
@@ -1,10 +1,10 @@
 extends MenuBar
 
 @onready var check_button: CheckButton = $multi/CheckButton
-@onready var check_color: CheckButton = $color_piezas/checkColor
+@onready var option_color: OptionButton = $color_piezas/option_color
 
-var check_color_elegido = true
-var pre_check_color = true
+var color_elegido = true
+var pre_color = true
 func _ready() -> void:
 	check_button.disabled = true
 
@@ -16,15 +16,17 @@ func _on_check_button_pressed() -> void:
 func _on_check_button_mouse_entered() -> void:
 	Sonidos.error()
 
-
-func _on_check_color_toggled(toggled_on: bool) -> void:
-	check_color_elegido = toggled_on
-
-
 func _on_guardar_pressed() -> void:
-	Piezas.color_piezas = check_color_elegido
+	Piezas.color_piezas = color_elegido
 
 
 func _on_restablecer_pressed() -> void:
-	check_color.button_pressed  = pre_check_color
-	check_color_elegido = pre_check_color
+	option_color.select(0)
+	color_elegido = pre_color
+
+
+func _on_option_color_item_selected(index: int) -> void:
+	if index == 0:
+		color_elegido = true
+	else:
+		color_elegido = false
