c6ca4cfd3b985dcc12bf7a494bbabcb6f4d4b7af
Heber Alonso <84638715+lu8dcf@users.noreply.github.com>
2026-06-30 10:04:12 -0300

    Update fabricaPiezas.gd
    Coloque un limitador en las cantiodades de piezas blancas que s epueden generar, en una match para evitar que la fabrica geenre mas piezas de las que corresponde

--- scenes/pieza/fabricaPiezas.gd
+++ scenes/pieza/fabricaPiezas.gd
@@ -9,6 +9,8 @@ var pieza_escena = preload("res://scenes/pieza/pieza_base.tscn")
 var id:int = 0  # id de la pieza
 var sitio3d: Vector3i
 
+
+
 func _ready() -> void:
 	GlobalSignal.connect("crearPieza",colocar_pieza) # Singleton
 	
@@ -17,16 +19,16 @@ func colocar_pieza(sitio: Vector2i, tipo: int , pieza_blanca: bool):
 	
 	if GlobalJuego.lugar_disponible(sitio)==false: # verifica que el lugar a instanciar este libre
 		return
-		
+	if pieza_blanca:	
+		if limite_pieza(tipo):  # verifica que no se puedan poner mas piezas		
+			return
 	# instanciar		
 	nueva_pieza = pieza_escena.instantiate()
 			
 	nueva_pieza.pieza_tipo = tipo
 	nueva_pieza.pieza_blanca = pieza_blanca 
 	nueva_pieza.pieza_sitio = sitio 
-	
-	
-	
+			
 	sitio3d = Vector3i(round(sitio.x * espaciado_baldosas), 10,round(sitio.y * espaciado_baldosas))
 	
 	# Asignar al grupo correspondiente
@@ -41,10 +43,38 @@ func colocar_pieza(sitio: Vector2i, tipo: int , pieza_blanca: bool):
 	add_child(nueva_pieza)
 	
 	nueva_pieza.global_position = sitio3d
-		
+	
+	
 		
 	
 	#print ((Piezas.pieza_activa[id].global_position)/GlobalJuego.espaciado_baldosas)
 	id += 1 # incrementador de nuemro de pieza
-	#print ("Blanca ",Piezas.contar_piezas_blancas())
-	#print ("Negras ",Piezas.contar_piezas_negras())
+	
+	
+func limite_pieza(tipo) -> bool:
+	var cantidad_a = 0
+	var limite = 0
+	for pieza in Piezas.pieza_blanca:
+		if pieza.pieza_tipo == tipo:
+			cantidad_a +=1
+	
+	match tipo:
+		0:
+			limite=1
+		1:
+			limite=8
+		2:
+			limite=2
+		3:
+			limite=2
+		4:
+			limite=2
+		5:
+			limite=1
+	
+	if cantidad_a >= limite:
+		return true
+	return false	
+	
+				
+			
